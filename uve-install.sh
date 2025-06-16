8f2fe038ee53be40a89d3e98a59409dd7bbfdae6
Shreeram <iamshreeram@users.noreply.github.com>
2025-06-16 21:04:21 +0530

    Update uve-install.sh
    Fixing the location of uve-binary

--- uve-install.sh
+++ uve-install.sh
@@ -16,15 +16,17 @@ case $ARCH in
     *)      echo "Unsupported architecture"; exit 1 ;;
 esac
 
+BIN_NAME="uve-${OS}-${ARCH}"
+
 # Download appropriate binary
 echo "Downloading UVE for ${OS}-${ARCH}..."
 case $OS in
     linux|darwin)
-        curl -L "${BASE_URL}/uve-${OS}-${ARCH}.tar.gz" -o "${TEMP_DIR}/uve.tar.gz"
+        curl -L "${BASE_URL}/${BIN_NAME}.tar.gz" -o "${TEMP_DIR}/uve.tar.gz"
         tar -xzf "${TEMP_DIR}/uve.tar.gz" -C "${TEMP_DIR}"
         BIN_PATH="${HOME}/.local/bin"
         mkdir -p "${BIN_PATH}"
-        mv "${TEMP_DIR}/uve-bin" "${BIN_PATH}/uve"
+        mv "${TEMP_DIR}/${BIN_NAME}" "${BIN_PATH}/uve"
         chmod +x "${BIN_PATH}/uve"
         ;;
     *)
